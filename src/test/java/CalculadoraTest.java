7ee4383340cd5d2a03aebf6a9a76aa8b9cbc6b19
Henrique Árabe Neres de Farias <118843050+HenriqueArabe@users.noreply.github.com>
2025-03-30 17:53:53 -0300

    Add files via upload

---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+
+    @Test
+    public void testSoma() {
+        Calculadora calc = new Calculadora();
+        assertEquals(5, calc.somar(2, 3));
+    }
+}
