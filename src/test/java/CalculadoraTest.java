75361aa6616487a5be3cf8ec1933e2f3fd5b17eb
Joaquim Rafael Prieto <joaquim.mpp@outlook.com>
2025-04-11 08:41:22 -0300

    Update CalculadoraTest.java

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -6,6 +6,11 @@ public class CalculadoraTest {
     @Test
     public void testSoma() {
         Calculadora calc = new Calculadora();
+        assertEquals(5, calc.somar(2, 3));
+    }
+    @Test
+    public void testSomaErro() {
+        Calculadora calc = new Calculadora();
         assertEquals(10, calc.somar(2, 3));
     }
 }
