ce74344dbe15a46606f0ffb6685dc2134455322b
Joaquim Rafael Prieto <joaquim.mpp@outlook.com>
2025-05-08 17:39:04 -0300

    Update CorsConfig.java

--- backEnd/src/main/java/com/exemplo/softwarelab/CorsConfig.java
+++ backEnd/src/main/java/com/exemplo/softwarelab/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500", "https://.com")
+                        .allowedOrigins("http://localhost:5500", "https://software-engineering-lab2.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
