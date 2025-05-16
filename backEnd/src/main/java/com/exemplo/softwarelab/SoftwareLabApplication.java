1b8f1cf046150d2cfd6b9e375d868348badf30f3
joaquimprieto <joaquim.mpp@outlook.com>
2025-05-16 07:27:31 -0300

    comments

--- backEnd/src/main/java/com/exemplo/softwarelab/SoftwareLabApplication.java
+++ backEnd/src/main/java/com/exemplo/softwarelab/SoftwareLabApplication.java
@@ -2,7 +2,7 @@ package com.exemplo.softwarelab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//init application
 @SpringBootApplication
 public class SoftwareLabApplication {
 
